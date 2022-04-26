90cf17ea6700e19c71fab3166f8fe45dc5c19a4b
Amaan <laskaramaan@gmail.com>
2022-04-26 20:48:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [newsList, setNewsList] =useState([])
+
+  useEffect(()=>{
+    fetch("https://api.spaceflightnewsapi.net/v3/articles")
+    .then((response)=> response.json())
+    .then((data)=> {
+      setNewsList(data)
+      console.log(data)
+    });
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='bg-gradient-to-r from-cyan-300 to-blue-300 '>
+      <div >
+        <h1 className="text-6xl font-bold ">Space News</h1>
+      </div>
+
+      <div className='newsContainer'>
+        {newsList.map((val,key)=>{
+          return (
+            <div key={key} className='article' > 
+              <h3 className='p-6 text-2xl font-bold'>{val.title}</h3>
+              <img src={val.imageUrl}/>
+              <p className='p-4'>{val.summary}</p>
+              <h4 className='pb-3'>{val.publishedAt}</h4>
+              <button className='p-3 rounded-full bg-lime-500 hover:bg-lime-700 mb-3 drop-shadow-lg' onClick={()=>{
+              window.location = val.url
+            }}>Read Full Story</button>
+            </div>
+          )
+
+
+        })}
+
+       
+
+      </div>
+      </div>
     </div>
   );
 }
